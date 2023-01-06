56e407bda92fb55894340bd908d60a0e4a7eab25
JingWei-S <weijing.524@gmail.com>
2023-01-06 12:17:47 +1100

    update the function that determines the win

--- game.js
+++ game.js
@@ -8,3 +8,27 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+// this function determines which party wins
+function playRound (playerSelection, computerSelection) {
+    // convert the playSelection
+    playerSelection = convertSelection(playerSelection);
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rocs')) {
+            return 'The player won!';
+        } else {
+            return 'The computer won!';
+        }
+
+}
+
+// test whether the playRound fucntion works
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+// convert the selection
+function convertSelection (playerSelection) {
+    return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
