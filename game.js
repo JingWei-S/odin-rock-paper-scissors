ee1e63a69d608185487ce4546963ddb7759582ef
JingWei-S <weijing.524@gmail.com>
2023-01-06 12:04:42 +1100

    update the getComputerChoice function

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randIndex = Math.floor(Math.random() * 3);
+    console.log(randIndex);
+    return choices[randIndex];
+}
+
+console.log(getComputerChoice());
